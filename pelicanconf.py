2772efddc6794a56f50c517baaa05f18f43bb421
Alexandros Kosiaris <akosiaris@gmail.com>
2015-09-20 23:46:53 +0300

    Add google search console verification
    Adding google search console, previously known as google webmaster's
    tools verification file

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,9 +29,13 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = [ 'images', 'extra/CNAME', 'extra/googlebf3ef559c8fe5d4e',
+]
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/googlebf3ef559c8fe5d4e': { 'path': 'googlebf3ef559c8fe5d4e.html'},
+}
 LOCALE='C'
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
